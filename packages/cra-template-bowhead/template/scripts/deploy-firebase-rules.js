7c4847737ee8c1ed45a61dc7c71d38fac1300897
David Morton <davidmortonmail@gmail.com>
2020-08-20 11:04:48 +0100

    test(cra-template-bowhead): test removing template files

--- packages/cra-template-bowhead/template/scripts/deploy-firebase-rules.js
+++
@@ -1,8 +0,0 @@
-const sh = require('shelljs')
-
-require('dotenv').config()
-
-if (sh.exec(`firebase deploy --only firestore:rules --token '${process.env.FIREBASE_TOKEN}' --project ${process.env.FIREBASE_PROJECT_ID} --non-interactive`).code !== 0) {
-    sh.echo('Error: Firebase deploy falied');
-    sh.exit(1);
-}
