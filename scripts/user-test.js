9aece400eee075975a45b9687c2241286029a49c
David Morton <davidmortonmail@gmail.com>
2020-08-28 11:48:36 +0100

    fix(*): fixing tests

--- scripts/user-test.js
+++ scripts/user-test.js
@@ -1,5 +1,10 @@
 const sh = require('shelljs')
 
+if (sh.rm('-rf', './temp/my-app').code !== 0) {
+    sh.echo('Error: Removing temp directory');
+    sh.exit(1);
+}
+
 if (sh.exec(`npx create-react-app temp/my-app --template file:packages/cra-template-bowhead`).code !== 0) {
     sh.echo('Error: user test');
     sh.exit(1);
