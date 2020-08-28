11e59e6506c4a358bdafc88328aad88724d48a04
David Morton <davidmortonmail@gmail.com>
2020-08-28 17:33:37 +0100

    fix(test-app): fixed some tests

--- packages/test-app/src/functions/webhook-stripe.test.js
+++ packages/test-app/src/functions/webhook-stripe.test.js
@@ -1,6 +1,5 @@
 import testFunction from './webhook-stripe';
 import { stripe } from '../utils/backend/stripeBackend'
-// import { firestore } from '../utils/backend/firebaseBackend';
 
 jest.mock('../utils/backend/firebaseBackend')
 jest.mock('../utils/backend/stripeBackend')
@@ -19,7 +18,6 @@ test('should return 401 when stripe.webhooks.constructEvent fails', async () => 
     const event = { headers: { 'stripe-signature': 'test' } }
 
     // when
-    console.log(testFunction.dbUpdateCustomerData)
     await testFunction.handler(event, null, callback);
 
     // then
