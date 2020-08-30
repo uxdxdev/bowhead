9cef4149a894b4739e339fc7864d40210ead0e37
David Morton <davidmortonmail@gmail.com>
2020-08-30 13:03:36 +0100

    refactor(*): lots of refactoring

--- packages/test-app/src/App.js
+++ packages/test-app/src/App.js
@@ -1,60 +1,10 @@
 import React, { useMemo } from "react";
-import { configureStore } from "@reduxjs/toolkit";
-import { Provider, useSelector } from "react-redux";
-import {
-  createFirestoreInstance,
-  reduxFirestore,
-} from "redux-firestore";
-import {
-  ReactReduxFirebaseProvider,
-  isLoaded,
-} from "react-redux-firebase";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
 import { useMediaQuery, CssBaseline } from "@material-ui/core";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
-// custom
-import rootReducer from "./store/rootReducer";
-import { firebase } from "./utils/frontend/firebaseFrontend";
-import { FIRESTORE_COLLECTIONS } from "./utils/constants";
-import { AuthenticatedRoute, PageLoadingSpinner } from "./components";
+import { AuthenticatedRoute, AuthIsLoaded } from "./components";
 import { LandingPage, SignIn, Verify, Dashboard, Terms } from "./pages";
-
-const store = configureStore(
-  {
-    reducer: rootReducer,
-    // middleware: [thunk] // default
-    enhancers: [
-      // enhancements to connect redux to Firebase
-      // pass in the firebase config details
-      reduxFirestore(firebase, {
-        logListenerError: false,
-      })]
-  },
-)
-
-const reactReduxFirebaseConfig = {
-  firebase,
-  // required to use firestore listeners in react-redux-firebase
-  createFirestoreInstance,
-  // sync the user data stored in firestore
-  // with the firebase redux state
-  // this avoids having to deconstruct the snapshot map
-  // of the user data stored in firestore
-  config: {
-    userProfile: FIRESTORE_COLLECTIONS.USERS,
-    useFirestoreForProfile: true,
-  },
-  dispatch: store.dispatch,
-};
-
-// do not render the app until Firebase auth is ready
-// http://react-redux-firebase.com/docs/recipes/auth.html#wait-for-auth-to-load
-const AuthIsLoaded = ({ children }) => {
-  const auth = useSelector((state) => state.firebase.auth);
-  if (!isLoaded(auth)) return <PageLoadingSpinner />;
-  return children;
-};
+import { StoreProvider } from './store'
 
 const App = () => {
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
@@ -115,24 +65,22 @@ const App = () => {
   );
 
   return (
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <ReactReduxFirebaseProvider {...reactReduxFirebaseConfig}>
-          <AuthIsLoaded>
-            <CssBaseline />
-            <BrowserRouter>
-              <Switch>
-                <Route exact path="/" component={LandingPage} />
-                <Route path="/terms" component={Terms} />
-                <Route path="/signin" component={SignIn} />
-                <Route path="/verify" component={Verify} />
-                <AuthenticatedRoute path="/dashboard" component={Dashboard} />
-              </Switch>
-            </BrowserRouter>
-          </AuthIsLoaded>
-        </ReactReduxFirebaseProvider>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <StoreProvider>
+        <AuthIsLoaded>
+          <CssBaseline />
+          <BrowserRouter>
+            <Switch>
+              <Route exact path="/" component={LandingPage} />
+              <Route path="/terms" component={Terms} />
+              <Route path="/signin" component={SignIn} />
+              <Route path="/verify" component={Verify} />
+              <AuthenticatedRoute path="/dashboard" component={Dashboard} />
+            </Switch>
+          </BrowserRouter>
+        </AuthIsLoaded>
+      </StoreProvider>
+    </ThemeProvider>
   );
 };
 
---
+++ packages/test-app/src/components/auth-is-loaded/index.js
@@ -0,0 +1,1 @@
+export { default as AuthIsLoaded } from './auth-is-loaded'
