0a00f261d84eea14467a5e652048b43c7f78f380
David Morton <davidmortonmail@gmail.com>
2020-09-11 18:52:23 +0100

    fix(test-app): updates for env variables

--- packages/cra-template-bowhead/template/scripts/deploy-firebase-rules.js
+++ packages/cra-template-bowhead/template/scripts/deploy-firebase-rules.js
@@ -2,7 +2,7 @@ const sh = require('shelljs')
 
 require('dotenv').config()
 
-if (sh.exec(`firebase deploy --only firestore:rules --token '${process.env.FIREBASE_TOKEN}' --project ${process.env.FIREBASE_PROJECT_ID} --non-interactive`).code !== 0) {
+if (sh.exec(`firebase deploy --only firestore:rules --token '${process.env.FIREBASE_TOKEN}' --project ${process.env.REACT_APP_FIREBASE_PROJECT_ID} --non-interactive`).code !== 0) {
     sh.echo('Error: Firebase deploy falied');
     sh.exit(1);
 }
